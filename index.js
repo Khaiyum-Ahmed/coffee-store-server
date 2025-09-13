51dd127f6be6de408a9d533e901256721f3f7aaf
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2025-09-13 16:19:00 +0600

    delete item added to the server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -47,6 +47,12 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/coffee/:id',async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await coffeeCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
         // Send a ping to confirm a successful connection
