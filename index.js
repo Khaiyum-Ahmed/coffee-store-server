991336e56dc9f896e86b992158b4a1213f6a07c9
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2025-09-16 01:31:52 +0600

    uncomment cors

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const port = process.env.PORT || 5000;
 
 
 //  middleware
-// app.use(cors({origin:["https://coffee-store-client-mauve.vercel.app","http://localhost:5173"]}));
-app.use(cors());
+app.use(cors({origin:["https://coffee-store-client-mauve.vercel.app","http://localhost:5173"]}));
+// app.use(cors());
 app.use(express.json());
 
 
